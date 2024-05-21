1c3ccb8d7fe9e06ba524cc5a2a263f39b3b3151c
Skye <lewallen4@gmail.com>
2024-05-21 16:52:07 -0500

    fix

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo ""
 echo ""
 echo ""
 echo "first login as sudo"
-sudo apt-get --yes --force-yes install firewalld openssh-client openssh-server
+sudo apt-get --yes install firewalld openssh-client openssh-server
 echo "installing firewalld & openssh"
 echo "launching script"
 sudo python3 st33l.py
