1cea4338185215335e290c21a202418123c705f5
Skye <lewallen4@gmail.com>
2024-05-21 18:59:36 -0500

    it works, now playing with shell

--- ssl/server.sh
+++ ssl/server.sh
@@ -16,13 +16,16 @@ fi
 
 # Start OpenSSL server in the background
 echo "Starting OpenSSL server..."
-openssl s_server -quiet -key server.pem -cert server.pem -port 9812 -WWW &
+# Use `stdbuf -i0 -o0 -e0` to make sure the I/O is not buffered
+(stdbuf -i0 -o0 -e0 openssl s_server -quiet -key server.pem -cert server.pem -port 9812) &
 
 # Wait for the server to start
 sleep 2
 
 # Connect to the server from the client
 echo "Connecting to OpenSSL server..."
-openssl s_client -connect localhost:9812 -quiet <<EOF
+# Use `-ign_eof` to ensure the connection doesn't close
+openssl s_client -connect localhost:9812 -quiet -ign_eof
+
+# Provide a shell on the server side
 /bin/sh -i
-EOF
